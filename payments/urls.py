dad17cf243a33dcae3ee245b7ce450f51b0ed8ce
Moeen_10 <kmoeen3233@gmail.com>
2026-04-13 22:37:14 +0600

    Add remaining project files: payments, settings, user control, email apps, and utility scripts

---
+++ payments/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import initiate_payment, payment_success, payment_fail, payment_cancel, VerifyPaymentView, payment_list_create
+
+urlpatterns = [
+    path('initiate/', initiate_payment, name='initiate_payment'),
+    path('success/', payment_success, name='payment_success'),
+    path('fail/', payment_fail, name='payment_fail'),
+    path('cancel/', payment_cancel, name='payment_cancel'),
+    path('verify_payment/', VerifyPaymentView.as_view(), name='verify_payment'),
+    path('list/', payment_list_create, name='payment_list'),
+] 
